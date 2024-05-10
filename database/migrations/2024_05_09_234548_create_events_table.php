d87da4471fd6ca15e223992ab822078c88943f3f
Samuel Cardenas <samuelcardenas1706@gmail.com>
2024-05-10 01:25:32 -0500

    Add relationships and implenet migrations

--- database/migrations/2024_05_09_234548_create_events_table.php
+++ database/migrations/2024_05_09_234548_create_events_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use App\Models\User;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -15,6 +16,14 @@ return new class extends Migration
     {
         Schema::create('events', function (Blueprint $table) {
             $table->id();
+
+            $table->foreignIdFor(User::class);
+            $table->string('name');
+            $table->text('description')->nullable();
+
+            $table->dateTime('start_time');
+            $table->dateTime('end_time');
+
             $table->timestamps();
         });
     }
