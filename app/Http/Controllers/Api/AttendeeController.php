0127bf8ba6dc339dd8060cfa9ffb04e47eaa1b53
Samuel Cardenas <samuelcardenas1706@gmail.com>
2024-07-17 15:58:17 -0500

    Implement AttendeeController logic and add pagination in Event and Attendee controller

--- app/Http/Controllers/Api/AttendeeController.php
+++ app/Http/Controllers/Api/AttendeeController.php
@@ -3,62 +3,65 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\AttendeeResource;
+use App\Models\Attendee;
+use App\Models\Event;
 use Illuminate\Http\Request;
 
 class AttendeeController extends Controller
 {
     /**
      * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
+     * @param \App\Models\Event $event
+     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
      */
-    public function index()
+    public function index(Event $event)
     {
-        //
+        // Get the attendees
+        $attendees = $event->attendees()->latest();
+
+        // Display Attendees with pagination
+        return AttendeeResource::collection(
+            $attendees->paginate()
+        );
     }
 
     /**
      * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Models\Event $event
+     * @return AttendeeResource
      */
-    public function store(Request $request)
+    public function store(Request $request, Event $event)
     {
-        //
+        $attendee = $event->attendees()->create([
+            'user_id' => 1,
+        ]);
+
+        return new AttendeeResource($attendee);
     }
 
     /**
      * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param \App\Models\Event $event
+     * @param \App\Models\Attendee $attendee
+     * @return AttendeeResource
      */
-    public function show($id)
+    public function show(Event $event, Attendee $attendee)
     {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
+        return new AttendeeResource($attendee);
     }
 
     /**
      * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param string $event
+     * @param \App\Models\Attendee $attendee
+     * @return \Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(string $event, Attendee $attendee)
     {
-        //
+        $attendee->delete();
+
+        return response(status: 204);
     }
 }
