5a712dcc0965117c61ec9e6528f03d2e85d3691e
Samuel Cardenas <samuelcardenas1706@gmail.com>
2024-09-19 17:54:44 -0500

    Remind about events to attendees sending email

--- app/Console/Commands/SendEventReminders.php
+++ app/Console/Commands/SendEventReminders.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use App\Models\Event;
+use App\Notifications\EventReminderNotification;
 use Illuminate\Support\Str;
 use Illuminate\Console\Command;
 
@@ -40,7 +41,11 @@ class SendEventReminders extends Command
 
         $events->each(
             fn ($event) => $event->attendees->each(
-                fn ($attendee) => $this->info("Notifying the user {$attendee->user->id}")
+                fn ($attendee) => $attendee->user->notify(
+                    new EventReminderNotification(
+                        $event
+                    )
+                )
             )
         );
 
--- config/mail.php
+++ config/mail.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('MAIL_MAILER', 'smtp'),
+    'default' => env('MAIL_MAILER', 'mailhog'),
 
     /*
     |--------------------------------------------------------------------------
@@ -76,6 +76,14 @@ return [
                 'smtp',
                 'log',
             ],
+        ],
+        'mailhog' => [
+            'transport' => 'smtp',
+            'host' => '127.0.0.1',     // MailHog SMTP server host
+            'port' => 1025,            // MailHog SMTP server port
+            'encryption' => null,      // No encryption is used for MailHog
+            'username' => null,        // No username is required for MailHog
+            'password' => null,        // No password is required for MailHog
         ],
     ],
 
